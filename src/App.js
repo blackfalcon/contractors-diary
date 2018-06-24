b53376e42e927c8b035334f0825cbcc85c76ae39
Dale Sande <dale.sande@gmail.com>
2018-06-23 17:10:49 -0700

    address general layout and adds 404 catch

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React, { Component } from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import firebase from 'firebase';
 import 'reset-css';
 import './App.css';
 import Header from './components/header/Header';
 import Splash from './components/splash/Splash';
+import PageNotFound from './components/pageNotFound/PageNotFound';
 
 const auth = firebase.auth();
 const provider = new firebase.auth.GoogleAuthProvider();
@@ -11,11 +13,19 @@ const provider = new firebase.auth.GoogleAuthProvider();
 class App extends Component {
   render() {
     return (
-      <main>
-        <Header auth={auth} provider={provider}/>
+      <Router>
+        <section>
+          <Header auth={auth} provider={provider}/>
+          <main className='main-container'>
+            <Switch>
 
-        <Splash auth={auth} />
-      </main>
+              <Route exact path='/' render={() => <Splash auth={auth} />} />
+              <Route component={PageNotFound}/>
+
+            </Switch>
+          </main>
+        </section>
+      </Router>
     );
   }
 }
---
+++ src/components/pageNotFound/PageNotFound.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class PageNotFound extends Component {
+
+  render() {
+    return (
+      <p>Sorry folks ... Park's Closed!</p>
+    );
+  }
+}
+export default PageNotFound;
